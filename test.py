d818c0911a2148052a7c8650d751feb638058632
Winthrop Gillis <win.gillis@gmail.com>
2014-01-25 15:56:10 -0500

    Fixed main page error

--- test.py
+++ test.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/<html>')
-def hello(html):
-	return render_template(html +'.html')
+def hello(html=None):
+	if not html:
+		return "Hello world!"
+	else:
+		return render_template(html +'.html')
 
 if __name__== '__main__':
 	app.run()
