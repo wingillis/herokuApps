d17a589274a3ade762aa7e34fe3b923e09f91475
Winthrop Gillis <win.gillis@gmail.com>
2014-01-25 15:43:43 -0500

    Added html templates and folder for css

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return 'Hello world!'
+@app.route('/<html>')
+def hello(html):
+	return render_template(html)
+
+if __name__== '__main__':
+	app.run()
